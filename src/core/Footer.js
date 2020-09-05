76edd7145b29876efce3b5a45ac70322e9b87086
shivanshu5416jais <42492030+shivanshu5416jais@users.noreply.github.com>
2020-09-05 17:41:53 +0530

    some changes done

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -8,7 +8,7 @@ import {signout,isAuthenticated} from "../auth"
 
 const Footer=()=>(
   <>
-  <div className="" style={{margin:"8px",fontSize:"30px",color:"Red",float:"bottom",backgroundColor:"black",width:"100vw",textAlign:"center",marginTop:"2vh",
+  <div className="" style={{fontSize:"30px",color:"Red",float:"bottom",backgroundColor:"black",width:"100vw",textAlign:"center",marginTop:"2vh",
   bottom: "10px"}}>
      <center>Made By Shivanshu Jaiswal</center>
     </div>
